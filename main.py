7387a162c9a33027cc376a71a92d090b471d8ad6
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-26 15:35:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+from config import (
+    API_ID,
+    API_HASH,
+    BOT_TOKEN
+)
+
+bot = Client(
+    "QR CODE BOT",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins={
+        "root": "bot/plugins"
+    },
+    parse_mode="html"
+)
+bot.run()
