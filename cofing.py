32f93335c09ebea2641c693edff025fc446d6c7c
AIOM BOTS <96116647+aiombots@users.noreply.github.com>
2022-04-26 20:31:06 +0530

    Create cofing.py

---
+++ cofing.py
@@ -0,0 +1,7 @@
+import os
+# from @botfather
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# from https://my.telegram.org/apps
+API_ID = int(os.environ.get("API_ID"))
+# from https://my.telegram.org/apps
+API_HASH = os.environ.get("API_HASH")
